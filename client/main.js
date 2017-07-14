10a71ccc5cd8800e5eba0225a54009915a45cbe6
blu77 <coryzhou2001@yahoo.com>
2017-07-14 15:16:06 -0400

    commented stuff
    its safe

--- client/main.js
+++ client/main.js
@@ -6,3 +6,17 @@ Template.main.events({
   },
 
   })
+
+/*setInterval(settime, 5000);
+
+//window.onload = settime
+
+function settime(){
+  document.getElementById("functiontest").innerHTML = Math.round(Math.random()*1000)
+}
+
+function loaded() {
+    if (window.location.hash == '#open') alert('ok')
+}
+window.location.hash = 'open'
+window.onload = loaded;*/
