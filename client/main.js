a0d18cd56285c8171a4d6189cb06ea576294f276
vsharma3600 <vsharma3600@gmail.com>
2017-07-17 14:53:52 -0400

    counter

--- client/main.js
+++ client/main.js
@@ -1,10 +1,23 @@
 Template.main.events({
   "click #js-button"(event,instance){
     console.log("hey it worked");
+    var z = UserInfo.find().fetch()
+    console.dir(z)
     UserInfo.insert({user:Meteor.userId(),when:moment().format('MMMM Do YYYY, h:mm:ss a')})
     location.reload();
 
   },
+
+  })
+  Template.main.helpers({
+    clicks(){
+      return UserInfo.find().count();
+    }
+  })
+
+
+  Template.previouswinners.helpers({
+    users(){return Pwinners.find({},{limit:10,sort:{when:-1}})},
 
   })
 
